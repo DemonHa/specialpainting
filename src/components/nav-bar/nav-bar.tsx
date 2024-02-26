47bef812d6f59c068091ca2a2a939a00337dad9d
DemonHa <hh360284@gmail.com>
2024-02-26 23:35:19 +0100

    feat: animate color of slider

--- src/components/nav-bar/nav-bar.tsx
+++ src/components/nav-bar/nav-bar.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import Link from "next/link";
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion, AnimatePresence } from "framer-motion";
 import NavBarLink from "./nav-bar-link";
 import AnimatedMenuButton from "./animated-menu-button";
 import { MdArrowOutward } from "react-icons/md";
@@ -12,35 +12,36 @@ const menuVariants = {
     height: 0,
     transition: {
       duration: 0.5,
-      ease: 'easeInOut'
-    }
+      ease: "easeInOut",
+    },
   },
   open: {
-    height: '100dvh', // Target height for the open state
+    height: "100dvh", // Target height for the open state
     transition: {
       duration: 0.5,
-      ease: 'easeInOut',
-    }
-  }
+      ease: "easeInOut",
+    },
+  },
 };
 
-const menuTitleStyles = "h-[15vh] flex justify-start items-end py-3 px-7 text-2xl border-b border-slate-700";
-const menuLinkStyles = "h-[10vh] flex justify-start items-end py-3 px-7 text-2xl border-b border-slate-700 text-orange-300 cursor-pointer"
+const menuTitleStyles =
+  "h-[15vh] flex justify-start items-end py-3 px-7 text-2xl border-b border-slate-700";
+const menuLinkStyles =
+  "h-[10vh] flex justify-start items-end py-3 px-7 text-2xl border-b border-slate-700 text-orange-300 cursor-pointer";
 
 const Navbar = () => {
-
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     if (open) {
-      document.body.style.overflow = 'hidden';
+      document.body.style.overflow = "hidden";
     } else {
-      document.body.style.overflow = '';
+      document.body.style.overflow = "";
     }
 
     // Optional: Cleanup function to ensure scrolling is enabled when the component unmounts
     return () => {
-      document.body.style.overflow = '';
+      document.body.style.overflow = "";
     };
   }, [open]);
 
@@ -48,7 +49,9 @@ const Navbar = () => {
     <div>
       <div className="flex justify-between w-full border-b border-slate-700 divide-x divide-slate-700 z-20 h-[7dvh] md:h-[7dvh] sm:h-[7dvh] lg:h-[7dvh] xl:h-[8dvh]">
         <div className="flex-1 flex items-center">
-          <Link href="/"><div className="p-5 text-xl">VENTION</div></Link>
+          <Link href="/">
+            <div className="p-5 text-xl">VENTION</div>
+          </Link>
         </div>
         <div className="flex xl:hidden border-l border-slate-700 min-w-[50px]:">
           <AnimatedMenuButton setOpen={setOpen} open={open} />
@@ -71,21 +74,55 @@ const Navbar = () => {
             exit="closed"
             variants={menuVariants}
             className="z-10 absolute w-full overflow-hidden flex xl:hidden"
-            style={{ top: '7dvh' }} // Adjust accordingly
+            style={{ top: "7dvh" }} // Adjust accordingly
           >
             <div className="w-[30vw] h-[93dvh] bg-glass-bg bg-opacity-35 shadow-glass backdrop-blur-glass border border-glass-border"></div>
             <div className="bg-black flex flex-col justify-between h-[93dvh] w-[70vw]">
               <div>
                 {/* Content goes here */}
                 <div className={menuTitleStyles}>What we do</div>
-                <Link onClick={() => {setOpen((prev) => !prev)}} href="portfolio"><div className={menuLinkStyles}>Portfolio</div></Link>
-                <Link onClick={() => {setOpen((prev) => !prev)}} href="what-we-do"><div className={menuLinkStyles}>Our Work</div></Link>
+                <Link
+                  onClick={() => {
+                    setOpen((prev) => !prev);
+                  }}
+                  href="portfolio"
+                >
+                  <div className={menuLinkStyles}>Portfolio</div>
+                </Link>
+                <Link
+                  onClick={() => {
+                    setOpen((prev) => !prev);
+                  }}
+                  href="what-we-do"
+                >
+                  <div className={menuLinkStyles}>Our Work</div>
+                </Link>
                 <div className={menuTitleStyles}>About Us</div>
-                <Link onClick={() => {setOpen((prev) => !prev)}} href="about-us"><div className={menuLinkStyles}>Reviews</div></Link>
-                <Link onClick={() => {setOpen((prev) => !prev)}} href="insights"><div className={menuLinkStyles}>Insights</div></Link>
+                <Link
+                  onClick={() => {
+                    setOpen((prev) => !prev);
+                  }}
+                  href="about-us"
+                >
+                  <div className={menuLinkStyles}>Reviews</div>
+                </Link>
+                <Link
+                  onClick={() => {
+                    setOpen((prev) => !prev);
+                  }}
+                  href="insights"
+                >
+                  <div className={menuLinkStyles}>Insights</div>
+                </Link>
               </div>
               {/* Fixed Button at the Bottom */}
-              <Link onClick={() => {setOpen((prev) => !prev)}} href="contact" className="px-7 py-3 bg-orange-600 mt-auto w-full flex justify-between items-center">
+              <Link
+                onClick={() => {
+                  setOpen((prev) => !prev);
+                }}
+                href="contact"
+                className="px-7 py-3 bg-orange-500 mt-auto w-full flex justify-between items-center"
+              >
                 <div className="text-3xl m-2">Contact Us</div>
                 <MdArrowOutward size={40} />
               </Link>
@@ -93,7 +130,6 @@ const Navbar = () => {
           </motion.div>
         )}
       </AnimatePresence>
-
     </div>
   );
 };
--- src/features/home/hero/hero.tsx
+++ src/features/home/hero/hero.tsx
@@ -5,13 +5,14 @@ import { MdArrowOutward } from "react-icons/md";
 import AnimatedSlider from "./animated-slider";
 import TextAnimationWrapper from "./text-animation";
 
-const template = [
+const templates = [
   {
     title: "Built to scale",
     description:
       "Vention developers partner with innovative companies from startups to Fortune 500s, lending the AI engineering expertise to propel them to new heights and the edge to outpace the competition.",
     button: "Get an estimate",
     color: "bg-orange-500",
+    hex: "#f97315",
   },
   {
     title: "AI innovation",
@@ -19,6 +20,7 @@ const template = [
       "As pioneers in AI engineering, we view it as more than a shiny tool: it's a pillar of the new business normal. Those companies that embrace and leverage AI will set the standard and lead in their category. In our new State of AI report, we look at funding, investment, and valuation trends across the AI market.",
     button: "Read The Report",
     color: "bg-green-400",
+    hex: "#49de80",
   },
   {
     title: "Expert software development",
@@ -26,6 +28,7 @@ const template = [
       "Get it done right — the first time. Our proven software expertise and engagement flexibility mean you don't have to worry about timelines, processes, or outcomes. Proficient in web, mobile, cloud, and advanced tech like AI and VR, we empower you to innovate and grow your business at the scale and speed of future trends.",
     button: "Explore Services",
     color: "bg-fuchsia-400",
+    hex: "#e979f9",
   },
   {
     title: "Exceptional customer service",
@@ -33,6 +36,7 @@ const template = [
       "At Vention, unparalleled customer care isn't just a promise — it's our mission. We blend personalized experience, unwavering attention to detail, and a genuine passion for your business, ensuring that every aspect of our collaboration is tailored to your success.",
     button: "Schedule a call",
     color: "bg-indigo-600",
+    hex: "#5046e5",
   },
 ];
 
@@ -41,40 +45,58 @@ const Hero = () => {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setPage((current) => (current === template.length - 1 ? 0 : current + 1));
+      setPage((current) =>
+        current === templates.length - 1 ? 0 : current + 1
+      );
     }, 5000);
 
     return () => clearInterval(interval);
   }, [page]);
 
-  const temp = template[page];
+  const template = templates[page];
 
   return (
     <>
       <div className="grid grid-cols-[3fr_13fr] md:grid-cols-[4fr_14fr] lg:grid-cols-2 xl:grid-rows-[auto_max-content] lg:grid-rows-[280px_max-content] gap-0 p-5 md:pt-14 lg:pt-5">
         <div
-          className={`flex flex-1 min-h-[200px] lg:min-h-[calc(143px+136*((100vw-1080px)/840))] items-center ${temp.color}`}
+          className={`flex flex-1 min-h-[200px] lg:min-h-[calc(143px+136*((100vw-1080px)/840))] items-center ${template.color}`}
         >
-          <TextAnimationWrapper text={temp.title} className="hidden lg:flex px-4 text-5xl xl:text-6xl" />
+          <TextAnimationWrapper
+            text={template.title}
+            className="hidden lg:flex px-4 text-5xl xl:text-6xl"
+          />
         </div>
         <div className="flex flex-1 lg:items-end justify-end lg:py-7">
           <div className="hidden lg:flex">
-            <AnimatedSlider activePage={page} setActivePage={setPage} />
+            <AnimatedSlider
+              color={template.hex}
+              activePage={page}
+              setActivePage={setPage}
+            />
           </div>
           <div className="flex flex-col min-h-[540px] md:min-h-[729px] pt-20 pb-8 pl-10 pr-5 md:pt-72 md:pl-32 md:pb-24 lg:hidden gap-4">
-            <TextAnimationWrapper className="text-4xl sm:text-5xl" text={temp.title} />
-            <TextAnimationWrapper text={temp.description} />
+            <TextAnimationWrapper
+              className="text-4xl sm:text-5xl"
+              text={template.title}
+            />
+            <TextAnimationWrapper text={template.description} />
           </div>
         </div>
         <div>
           <div className="hidden lg:flex h-[100%]">
             <div className="flex-1"></div>
             <div className="flex-1 min-w-[350px] flex flex-col justify-end pr-10">
-              <TextAnimationWrapper className="text-sm xl:text-lg" text={temp.description}/>
+              <TextAnimationWrapper
+                className="text-sm xl:text-lg"
+                text={template.description}
+              />
               <button
-                className={`w-full ${temp.color} mt-4 xl:mt-8 px-3 xl:px-6 py-2 xl:py-4 flex justify-between items-center`}
+                className={`w-full ${template.color} mt-4 xl:mt-8 px-3 xl:px-6 py-2 xl:py-4 flex justify-between items-center`}
               >
-                <TextAnimationWrapper className="text-sm xl:text-2xl" text={temp.button}/>
+                <TextAnimationWrapper
+                  className="text-sm xl:text-2xl"
+                  text={template.button}
+                />
                 <MdArrowOutward size={30} />
               </button>
             </div>
@@ -91,14 +113,19 @@ const Hero = () => {
             />
           </div>
           <div className="absolute bottom-0 right-0 lg:hidden">
-            <AnimatedSlider small activePage={page} setActivePage={setPage} />
+            <AnimatedSlider
+              color={template.hex}
+              small
+              activePage={page}
+              setActivePage={setPage}
+            />
           </div>
         </div>
       </div>
       <button
-        className={`lg:hidden w-full ${temp.color} px-3 py-6 flex justify-between items-center`}
+        className={`lg:hidden w-full ${template.color} px-3 py-6 flex justify-between items-center`}
       >
-        <TextAnimationWrapper className="text-2xl" text={temp.button} />
+        <TextAnimationWrapper className="text-2xl" text={template.button} />
         <MdArrowOutward size={30} />
       </button>
     </>
