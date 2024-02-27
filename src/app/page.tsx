0a1b6b41a202f6e37455d7bd1daba585bf0c7397
Frajdi <frajdimalaj@gmail.com>
2024-02-27 01:37:06 +0100

    fe: added footer section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Main from "@/features/home/main";
 import Statistics from "@/features/home/main/statistics";
 import Testimonials from "@/features/home/main/testimonials";
 import Achivements from "@/features/home/main/achivements";
+import Footer from "@/features/home/footer";
 
 export default function Home() {
   return (
@@ -12,9 +13,12 @@ export default function Home() {
       <Main>
         <Description />
         <Statistics />
-        <Achivements />
         <Testimonials />
+        <Achivements />
       </Main>
+      <Footer>
+        <div>Continue here</div>
+      </Footer>
     </>
   );
 }
