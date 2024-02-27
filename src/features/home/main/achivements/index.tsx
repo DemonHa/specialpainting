de5d3c2bb06493bc6f39ba22f2d7ab3ac0ae63ea
Frajdi <frajdimalaj@gmail.com>
2024-02-27 13:36:58 +0100

    fix: responsivnes on achivements

--- src/features/home/main/achivements/index.tsx
+++ src/features/home/main/achivements/index.tsx
@@ -6,21 +6,9 @@ const images = [photo, photo, photo, photo, photo, photo];
 
 const Achivements = () => {
   return (
-    <div className="grid grid-cols-4">
-      {images.map((image, idx) => {
-        return (
-          <div
-            key={idx}
-            className="flex py-5 border border-slate-700 justify-center"
-          >
-            <Image
-              width={120}
-              height={120}
-              src={image}
-              alt={"achievement-img"}
-            />
-          </div>
-        );
+    <div className='grid grid-cols-2 lg:grid-cols-4'>
+      {images.map((image) => {
+        return <div className='flex py-10 border border-slate-700 justify-center'><Image width={100} height={100} src={image} alt={"achievement-img"} /></div>
       })}
     </div>
   );
