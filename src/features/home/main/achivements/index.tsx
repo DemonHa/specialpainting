0a382fc1b0bb84cd365beaa8793f0fcdbf14b29c
Frajdi <frajdimalaj@gmail.com>
2024-02-29 11:04:20 +0100

    fe: contact layout

--- src/features/home/main/achivements/index.tsx
+++ src/features/home/main/achivements/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Image from 'next/image'
-import photo from "../../../../../public/photo.png"
+import screenedApproved from "@/../../public/screened_approved.png"
+import topRated  from "@/../../public/top_rated.png" 
+import eliteService from "@/../../public/elite_service.png";
 
-const images = [photo, photo, photo, photo, photo, photo]
+const images = [screenedApproved, topRated, eliteService, screenedApproved, screenedApproved, screenedApproved]
 
 const Achivements = () => {
   return (
     <div className='grid grid-cols-2 lg:grid-cols-4'>
       {images.map((image) => {
-        return <div className='flex py-10 border border-slate-700 justify-center'><Image width={100} height={100} src={image} alt={"achievement-img"} /></div>
+        return <div className='flex py-10 border border-slate-700 justify-center'><Image className='w-[120px] h-[120px]' priority={false} src={image} alt={"achievement-img"} /></div>
       })}
     </div>)
 }
